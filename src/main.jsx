5d327b3bf13992911e1904f9395a99583f8e6019
IronFist482 <icernap2000@alumno.ipn.mx>
2025-10-21 20:49:59 -0600

    Cerna Perez Ian Daniel // Arreglos a el ruteo de la página completa

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
-import Test from './pages/Test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Test />
   </StrictMode>,
 )
