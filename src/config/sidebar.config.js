97c9828d1477ef7ef636bb0785860daa6076d089
diegozzz06 <metd2006@gmail.com>
2025-10-29 18:47:38 -0600

    sidebar configuration
    Mueve la configuracion de sidebar para eliminar el hardcode

---
+++ src/config/sidebar.config.js
@@ -0,0 +1,10 @@
+// src/config/sidebar.config.js
+
+export const SIDEBAR_CONFIG = {
+  cookieName: "sidebar_state",
+  cookieMaxAge: 60 * 60 * 24 * 7, // 7 días
+  width: "16rem",
+  widthMobile: "18rem",
+  widthIcon: "3rem",
+  shortcut: "b",
+};
