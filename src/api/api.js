06f8e30e05681148db4910a31aa44eca5afa1e15
IronFist482 <icernap2000@alumno.ipn.mx>
2025-11-10 22:21:50 -0600

    Cerna Perez Ian Daniel // Uso de toast

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@
 const API_URL = 'https://proyectoalgebralineal-production.up.railway.app';
 //const API_URL = 'http://127.0.0.1:8000';
 
+import { toast } from 'react-toastify';
+
 
 export const procesarMatriz = async (operacion, archivo) => {
   const formData = new FormData();
@@ -20,11 +22,13 @@ export const procesarMatriz = async (operacion, archivo) => {
       throw new Error(errorData.detail || `Error HTTP: ${response.status}`);
     }
     console.log(response) ;
-
+    toast.success('Matriz procesada correctamente');
     return await response.json();
 
   } catch (error) {
+
     console.error('Error al llamar a la API:', error);
+    toast.error(`Error al procesar la matriz: ${error.message}`);
     throw error;
   }
 };
